8eb55b716f2c45df1dafca649721d85bfb7b134c
Daniela <daniela.londono@arbusta.net>
2022-08-02 12:02:23 -0300

    agrego la funcionalidad del buscador de pelicula y un brekpoint en boostrap para que se oculte el menu, a mejorar

--- js/infoPelicula.js
+++ js/infoPelicula.js
@@ -1,11 +1,15 @@
 $(()=>{
-    let titulo = window.sessionStorage.getItem('title');
-    let descripcion = window.sessionStorage.getItem('descripcion')
-    //window.sessionStorage.removeItem('url');
-    $('#tituloPeli').append(titulo);
+    //obtengo la descripcion guardada del sessionStorage y la agreago a mi html
+    let descripcion = window.sessionStorage.getItem('descripcion');
     $('#sipnosis').append(descripcion);
+
+
+    //obtengo la url con el search
     let datoImg = window.location.search;
-    $('#infoPeli').append(`<img class="imgInfoPeli" src="https://image.tmdb.org/t/p/w500/${datoImg.split('?url=').join('')}">`);
+    //lo agreo al html . convierto ese datoImg en array y lo divido por por el & y selecciono la  primera posicion despues de eso
+    //y despues sustituyo %20 por espacios
+    $('#tituloPeli').append(datoImg.split('&')[1].split('%20').join(' '));    
+    $('#infoPeli').append(`<img class="imgInfoPeli" src="https://image.tmdb.org/t/p/w500/${datoImg.split('?url=').join('').split('&')[0]}">`);
 
 
 
--- js/main.js
+++ js/main.js
@@ -1,21 +1,12 @@
 $(() => {
-
-
     $.ajax({
         type: 'GET',
         url: 'https://api.themoviedb.org/3/movie/popular?api_key=191528030c357419329af1198edbcb24',
         dataType: 'json'
     }).done(function (data) {
         mostrarDatos(data)
-        
     })
-
-  
-
-
-
 })
-
 
 /**
  * recibe el array de la api y la recorre
@@ -35,37 +26,51 @@ let mostrarDatos = (datos) => {
      `
     });
     $('#contenedor').append(peliculas);
-
-
 }
 
 
 
-let infoPelicula = (img,title,sipnosis) => {
-    window.sessionStorage.setItem('title',title);
-    window.sessionStorage.setItem('descripcion',sipnosis);
-    //let url = `https://image.tmdb.org/t/p/w500/${img}`;
-    window.location.href= './html/infoPeliculas.html?url='+img;
+let infoPelicula = (img, title, sipnosis) => {
+    //una de las formas de pasar info a otra pestaña html, es hacerla persistir y guaradarla en session o local estorage
+    window.sessionStorage.setItem('descripcion', sipnosis);
+
+    //esta es otra forma concatenando la info en la url usando el objeto window, haciendo que me lleve a la otra pagina
+    //con el location y propiedad href
+    window.location.href = './html/infoPeliculas.html?url=' + img + '&' + title;
 }
 
 
 
-
-
-
-
-
-
-
-
-
-/* input buscar */
-/*$('#formBuscar').on('submit',function(e){
+/* input buscar - evento de formulario */
+$('#formBuscar').on('submit', function (e) {
     e.preventDefault();
+    console.log('hola')
+    $.ajax({
+        type: 'GET',
+        url: 'https://api.themoviedb.org/3/movie/popular?api_key=191528030c357419329af1198edbcb24',
+        dataType: 'json'
+    }).done(function (data) {
+        console.log(data)
+        data.results.map(peli => {
+            if ( peli.title.toLowerCase().includes($('#txtBuscar').val().toLowerCase())) {
+                console.log(peli.title);
+                let cardPeli = `<div class="card" style="width: 18rem;">
+                <img src="https://image.tmdb.org/t/p/w500/${peli.poster_path}" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">${peli.title}</h5>
+                  <p class="card-text">${peli.overview}</p>
+                  <a href="#" class="btn btn-primary">Ver película</a>
+                </div>
+              </div>`;
+                $('#contenedor').html(cardPeli);
 
-    $('#txtBuscar').val()
-  })  
+            }/* else{   //no funciona
+                let noFound = '<p class="text-info fs-1">Película no encontrada</p>';
+                $('#contenedor').html(noFound);
+            } */
+        })
+    })
+})
 
-  let titulos = Array.from(document.querySelectorAll('.contenedor'));
-  
-  console.log(titulos) ; */
+
+
